76792777d49dc5ffb66cb309f3921b88f8cfb33e
Roman Tomin <tomrobryuc@gmail.com>
2024-05-01 17:41:55 +0300

    Fix #2.3 task_2 Attachments

--- src/main/kotlin/post/Post.kt
+++ src/main/kotlin/post/Post.kt
@@ -12,83 +12,102 @@ data class Post(
     val isPinned: Boolean,
     val likes: Likes,
     val date: Long,
-    val attachments: Attachment?
+    val attachments: ArrayList<Attachment?>?
 )
 
 
 interface Attachment {
     val type: String
-    val id: Int?
-    val ownerId: Int?
-    val title: String?
-    val url: String?
-    val date: Int?
+    val attachment: Any
 }
 
 
 class AudioAttachment(
     override val type: String = "audio",
-    override val id: Int,
-    override val ownerId: Int,
-    override val title: String?,
-    override val url: String,
-    override val date: Int,
+    override val attachment: Audio
+) : Attachment
+
+data class Audio(
+    val id: Int,
+    val ownerId: Int,
+    val title: String?,
+    val url: String,
+    val date: Int,
     val artist: String?,
     val duration: Int?,
     val lyricsId: Int?,
-    val description: String,
+    val description: String?,
     val genreId: Int?,
     val noSearch: Boolean,
     val isHQ: Boolean,
     val albumId: Int?
-) : Attachment
+)
 
 class DocumentAttachment(
     override val type: String = "document",
-    override val id: Int,
-    override val ownerId: Int,
-    override val title: String?,
-    override val url: String,
-    override val date: Int,
+    override val attachment: Document
+) : Attachment
+
+data class Document(
+    val id: Int,
+    val ownerId: Int,
+    val title: String?,
+    val url: String,
+    val date: Int,
     val size: Int,
     val ext: String,
     val docType: Int?
-) : Attachment
+)
+
 
 class LinkAttachment(
     override val type: String = "link",
-    override val id: Int,
-    override val ownerId: Int,
-    override val title: String?,
-    override val url: String,
-    override val date: Int,
+    override val attachment: Link
+) : Attachment
+
+data class Link(
+    val id: Int,
+    val ownerId: Int,
+    val title: String?,
+    val url: String,
+    val date: Int,
     val caption: String,
     val previewPage: String,
     val previewUrl: String
-) : Attachment
+)
+
 
 class PhotoAttachment(
     override val type: String = "photo",
-    override val id: Int,
-    override val ownerId: Int,
-    override val title: String?,
-    override val url: String,
-    override val date: Int,
+    override val attachment: Photo
+) : Attachment
+
+data class Photo(
+    val id: Int,
+    val ownerId: Int,
+    val title: String?,
+    val url: String,
+    val date: Int,
     val userId: Int,
     val text: String?,
     val width: Int,
     val height: Int,
     val description: String,
     val albumId: Int?
-) : Attachment
+)
+
 
 class StickerAttachment(
     override val type: String = "sticker",
-    override val id: Int? = null,
-    override val ownerId: Int? = null,
-    override val title: String? = null,
-    override val url: String? = null,
-    override val date: Int? = null,
+    override val attachment: Sticker
+) : Attachment
+
+data class Sticker(
+    val id: Int? = null,
+    val ownerId: Int? = null,
+    val title: String? = null,
+    val url: String? = null,
+    val date: Int? = null,
     val productId: Int,
     val stickerId: Int
-) : Attachment
+)
